c46def53f1cb8f1d143687336eb38567f408d6cd
cauan <cauanbarros14@gmail.com>
2023-04-06 14:14:27 -0300

    iniciando o css

---
+++ login-app/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>iniciando a tela de login!
+     </p>
+    </div>
+  );
+}
+
+export default App;
